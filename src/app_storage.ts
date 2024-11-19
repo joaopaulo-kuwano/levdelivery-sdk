9ef3716c585702f21f2f9114ddd1b2a2d857eb92
João Paulo Kuwano <joaopaulokuwano@gmail.com>
2024-11-19 13:15:37 -0300

    🚚arquivos template para novas rotas

--- src/app_storage.ts
+++ src/app_storage.ts
@@ -2,13 +2,11 @@ import { LocalStorage } from 'node-localstorage'
 
 let storage: Storage
 
-if (typeof window !== "undefined" && window.localStorage) {
-  // Running in the browser, use localStorage
+if (typeof window !== "undefined" && window.localStorage) 
   storage = window.localStorage;
-} else {
-  // Running in Node.js, use a Node-based localStorage alternative
+else
   storage = new LocalStorage('./scratch');
-}
+
 
 export class AppStorage {
   static setItem(key: string, value: string): void {
